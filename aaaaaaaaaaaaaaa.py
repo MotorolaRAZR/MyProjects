1131b3cd1d0505bcf271e495a1922ad4280020ff
MotorolaRAZR <95773403+MotorolaRAZR@users.noreply.github.com>
2025-01-25 18:23:52 +0300

    получение пдф расписания обход антибота

--- aaaaaaaaaaaaaaa.py
+++ aaaaaaaaaaaaaaa.py
@@ -6,7 +6,7 @@ cookies = {
     'dl': f'dl=dl.{a}.{b}.{c}',
 }
 
-
+#a
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'ru-RU,ru;q=0.9',
